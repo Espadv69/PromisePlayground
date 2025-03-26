7da46e07afd696a26b5097661bf158afd5ea5675
Espadv69 <aitorjob69@gmail.com>
2025-03-26 20:32:51 +0100

    Implement routing in App component with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import FunctionTester from './components/FunctionTester/FunctionTester'
 
 const App = () => {
   return (
-    <div className="app">
-      <FunctionTester />
-    </div>
+    <Router>
+      {/* Add navbar */}
+      <Routes>
+        <Route path="/" element={<FunctionTester />} />
+      </Routes>
+    </Router>
   )
 }
 
