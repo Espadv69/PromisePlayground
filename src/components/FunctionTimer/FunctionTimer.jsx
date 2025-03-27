2b1f5fe50137a8a6fca6a53675a08f576bc87ab8
Espadv69 <aitorjob69@gmail.com>
2025-03-27 11:19:59 +0100

    Add startTimerWithoutPromise function to FunctionTimer component

--- src/components/FunctionTimer/FunctionTimer.jsx
+++ src/components/FunctionTimer/FunctionTimer.jsx
@@ -5,6 +5,14 @@ const FunctionTimer = () => {
   const [message, setMessage] = useState('')
   let timeoutId = null
 
+  // Function without promises ✅
+  const startTimerWithoutPromise = (seconds) => {
+    setMessage(`Waiting for ${seconds} seconds...`)
+    timeoutId = setTimeout(() => {
+      setMessage(`Timer finished after ${seconds} seconds`)
+    }, seconds * 1000)
+  }
+
   // 🚀 Clear Timeout
   useEffect(() => {
     return () => clearTimeout(timeoutId)
