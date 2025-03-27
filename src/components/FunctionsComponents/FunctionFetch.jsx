7b96ec3fa7c84568f8991261499cbda66b19629a
Espadv69 <aitorjob69@gmail.com>
2025-03-27 20:36:40 +0100

    Remove mock data and fetchDataWithoutPromise function from FunctionFetch component

--- src/components/FunctionsComponents/FunctionFetch.jsx
+++ src/components/FunctionsComponents/FunctionFetch.jsx
@@ -1,24 +1,9 @@
 import { useState, useEffect } from 'react'
 import './Functions.css'
 
-const DATA = {
-  user: 'Aitor',
-  lastname: 'Díaz',
-  age: 21,
-}
-
 const FunctionFetch = () => {
   const [message, setMessage] = useState('')
   let timeoutId = null
-
-  // Function without promise ✅
-  const fetchDataWithoutPromise = () => {
-    setMessage('Fetching data...')
-
-    timeoutId = setTimeout(() => {
-      setMessage(DATA.user + ' ' + DATA.lastname + ' ' + DATA.age)
-    }, 2000)
-  }
 }
 
 export default FunctionFetch
