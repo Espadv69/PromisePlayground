5efca9738e3c5ffc291a2e8c331eaf07808c0d67
Espadv69 <aitorjob69@gmail.com>
2025-03-27 11:28:07 +0100

    Add FunctionTimer route to App component for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import NavBar from './components/NavBar/NavBar'
 import FunctionTester from './components/FunctionTester/FunctionTester'
+import FunctionTimer from './components/FunctionTimer/FunctionTimer'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<FunctionTester />} />
+        <Route path="/timer" element={<FunctionTimer />} />
       </Routes>
     </Router>
   )
