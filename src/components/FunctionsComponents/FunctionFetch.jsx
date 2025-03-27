8bd32bcdc7c941632da0d7f794d704bf5b648e62
Espadv69 <aitorjob69@gmail.com>
2025-03-27 20:39:38 +0100

    Add generateRandomNumberWithoutPromise function to generate random numbers without using promises

--- src/components/FunctionsComponents/FunctionFetch.jsx
+++ src/components/FunctionsComponents/FunctionFetch.jsx
@@ -4,6 +4,15 @@ import './Functions.css'
 const FunctionFetch = () => {
   const [message, setMessage] = useState('')
   let timeoutId = null
+
+  // ❌ Function without promise
+  const generateRandomNumberWithoutPromise = () => {
+    setMessage('Generating random number without promise...')
+    timeoutId = setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 100) + 1
+      setMessage(`Random number without promise: ${randomNumber}`)
+    }, 2000)
+  }
 }
 
 export default FunctionFetch
