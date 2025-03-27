c2e5877a824530e7091af920a2944012f5028aea
Espadv69 <aitorjob69@gmail.com>
2025-03-27 20:29:42 +0100

    Add useEffect and placeholder function for data fetching in FunctionFetch component

--- src/components/FunctionsComponents/FunctionFetch.jsx
+++ src/components/FunctionsComponents/FunctionFetch.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Functions.css'
 
 const FunctionFetch = () => {
   const [message, setMessage] = useState('')
+  let timeoutId = null
+
+  // Function without promise ✅
+  const fetchDataWithoutPromise = () => {}
 }
 
 export default FunctionFetch
