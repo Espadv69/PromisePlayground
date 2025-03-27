dc33410d9c6bbbf584b002c188ca7fbbdfdca768
Espadv69 <aitorjob69@gmail.com>
2025-03-27 20:42:25 +0100

    Add route for FunctionRandom component in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import FunctionTester from './components/FunctionsComponents/FunctionTester'
 import FunctionTimer from './components/FunctionsComponents/FunctionTimer'
+import FunctionRandom from './components/FunctionsComponents/FunctionRandom'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<FunctionTester />} />
         <Route path="/timer" element={<FunctionTimer />} />
+        <Route path="/random" element={<FunctionRandom />} />
       </Routes>
     </Router>
   )
