19fe727e2bfd1d56e4054545ca21e4711836308f
Espadv69 <aitorjob69@gmail.com>
2025-03-27 11:15:56 +0100

    Initialize state and timeoutId in FunctionTimer component

--- src/components/FunctionTimer/FunctionTimer.jsx
+++ src/components/FunctionTimer/FunctionTimer.jsx
@@ -1,5 +1,9 @@
+import { useState, useEffect } from 'react'
 import './FunctionTimer.css'
 
-const FunctionTimer = () => {}
+const FunctionTimer = () => {
+  const [message, setMessage] = useState('')
+  let timeoutId = null
+}
 
 export default FunctionTimer
